d26b7098933394309423c842060994c0ee6b9355
ivam98 <ivam98@mi.fu-berlin.de>
2021-04-25 21:50:56 +0200

    added POS and creation of output df

--- task_1/notebook.py
+++ task_1/notebook.py
@@ -1,11 +1,21 @@
 # %%
 import os
-import sys
 import pathlib
-
-import nltk
 import string
+import sys
 
+import gensim
+import nltk
+import numpy as np
+import pandas as pd
+import spacy
+import treetaggerwrapper
+
+from pattern.en import parse, split
+from pattern.en import pprint
+
+
+from utils.preprocess import clean_text, read_text, read_text_splitted
 
 # %%
 
@@ -13,74 +23,122 @@ import string
 # %autoreload 2
 
 
-from utils.preprocess import read_text, read_text_splitted
-
 # %%
 
 
 lang = "en"
-
 path_to_text = r"D:\Uni\Master\FU Data Science\Lectures\SS_21\NLP\project\task_1\data\Harry_en.txt"
+read_saved = True
 
 text = read_text_splitted(path_to_text)
+labeled_pos = pd.read_csv("tokens_POS_labeled.csv", index_col=0)
 
 
 # %%
 
+# prepare text
 
 last_5_sent_splitted = text[-5:]
-last_5_sent_full = " ".join(last_5_sent_splitted)
-last_5_sent_full_clean = "".join([i for i in last_5_sent_full if i not in string.punctuation.replace(".", "").replace("!","") + "”"])
-last_5_sent_full_clean = (
-    last_5_sent_full_clean.replace("That’ll", "That will")
-    .replace("Potter’s", "Potter is")
-    .replace("Voldy’s", "Voldy has")
-    .replace("let’s", "let us")
-)
 
+last_5_sent_full, last_5_sent_full_clean = clean_text(last_5_sent_splitted)
+print("Clean text:\n\n", last_5_sent_full_clean)
 
-print(last_5_sent_full_clean)
-
-manual_tokens = []
-
-for sent in last_5_sent_splitted:
-
-    # sent = sent.translate(str.maketrans('', '',string.punctuation))
-
-    sent = "".join([i for i in sent if i not in string.punctuation.replace(".", "")])  # leave dots
-
-    # sent = sent.replace("’ll", " will")  # .replace("’s")
-
-    sen_splitted = sent.split(" ")
-    manual_tokens.append(sen_splitted)
 
 # %%
 
-sum([len(i) for i in manual_tokens])
-manual_tokens
-# %%
 
+# tagging NLTK
 
-# tagging length
-
-print("NLTK tagging...\n")
-
+print("NLTK tokenization...\n")
 
 tags_nltk = nltk.word_tokenize(last_5_sent_full_clean)
-tags_nltk_clean = nltk.word_tokenize(last_5_sent_full_clean)
-print(f"Number of tags with punct: {len(tags_nltk)}")
-print(f"Number of tags without punct: {len(tags_nltk_clean)}")
-tags_nltk_clean
+print(f"Number of tokens: {len(tags_nltk)}")
+
+if not read_saved:
+    pd.DataFrame({"Tokens": tags_nltk}).to_csv("tokens_initial.csv")
+
+
+if len(tags_nltk) != labeled_pos.shape[0]:
+    print("inconsistency between nltk pos and labeled pos")
 
 
 # %%
+
+# Part of Speech NLTK
+
+
 pos_nltk = []
 
-for pos in nltk.pos_tag(tags_nltk_clean, lang="eng"):
-    pos_nltk.append(pos)
+for pos in nltk.pos_tag(tags_nltk, lang="eng"):
+    pos_nltk.append(pos[1])
 
-pos_nltk
+labeled_pos["NLTK_pos_pred"] = pos_nltk
+
 
 # %%
-nltk.word_tokenize("That'll be")
+
+# Part of Speech TreeTagger
+
+
+tagger = treetaggerwrapper.TreeTagger(
+    TAGDIR=r"D:\Uni\Master\FU Data Science\Lectures\SS_21\NLP\TreeTagger", TAGLANG=lang,
+)
+
+print(f"TreeTagger tokenization...\n")
+tags = tagger.tag_text(last_5_sent_full_clean)
+tags_treetagger = [w.split("\t")[1] for w in tags]
+print(f"Number of tokens: {len(tags_treetagger)}")
+
+if len(tags_treetagger) != labeled_pos.shape[0]:
+    print("inconsistency between nltk pos and labeled pos")
+
+labeled_pos["TreeTag_pos_pred"] = tags_treetagger
+
+
 # %%
+
+# Part of Speech Spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+doc = nlp(last_5_sent_full_clean)
+
+tokens_pos = []
+tokens_dep = []
+
+for token in doc:
+    tokens_pos.append(token.pos_)
+    tokens_dep.append(token.dep_)
+
+if len(tokens_pos) != labeled_pos.shape[0]:
+    print("inconsistency between spacy pos and labeled pos")
+
+labeled_pos["Spacy_pos_pred"] = tokens_pos
+
+# %%
+
+# Pattern
+
+s = parse(last_5_sent_full_clean)
+s = split(s)
+
+pattern_pos = []
+
+for i in range(len(s)):
+    pattern_pos = pattern_pos + list(s.sentences[i].pos)
+
+if len(pattern_pos) != labeled_pos.shape[0]:
+    print("inconsistency between pattern pos and labeled pos")
+
+labeled_pos["Pattern_pos_pos_pred"] = tokens_pos
+
+# %%
+
+[] + ["a"]
+
+
+# %%
+print(labeled_pos)
+
+labeled_pos.to_csv("pos_full_pred.csv", index=False)
+
--- task_1/utils/preprocess.py
+++ task_1/utils/preprocess.py
@@ -1,20 +1,39 @@
+import string
+
+
 def read_text(path_to_text: str) -> str:
-    
+
     with open(path_to_text, "r", encoding="utf-8") as f:
         text = f.read()
 
-    return text.replace("\n","")
+    return text.replace("\n", "")
 
 
 def read_text_splitted(path_to_text: str) -> str:
-    
+
     text = []
-    
+
     with open(path_to_text, "r", encoding="utf-8") as f:
         for line in f:
-            line_clean = line.replace("\n",'')
+            line_clean = line.replace("\n", "")
             if len(line_clean) < 2:
                 continue
             text.append(line_clean)
 
     return text
+
+
+def clean_text(text: str) -> list:
+
+    text_full = " ".join(text)
+    text_full_clean = "".join(
+        [i for i in text_full if i not in string.punctuation.replace(".", "").replace("!", "") + "”"]
+    )
+    text_full_clean = (
+        text_full_clean.replace("That’ll", "That will")
+        .replace("Potter’s", "Potter is")
+        .replace("Voldy’s", "Voldy has")
+        .replace("let’s", "let us")
+    )
+
+    return text_full, text_full_clean
